1f78a04bb29aae38f292b0f4576e1d2f1fae1bad
Gaurav-Kumar-Rana <32073848+Gaurav-Kumar-Rana@users.noreply.github.com>
2018-09-19 18:22:04 +0530

    Added New Files

---
+++ src/data_source/websettings.js
@@ -0,0 +1,13 @@
+// const defaultWebSettings = {
+//     website_Heading:"Portfolio",
+// };
+
+const defaultWebSettings = {
+    website_heading:"Sample",
+    wbsite_footer:{
+        title:"Folllow on",
+        sections:["Social","Community","Email","Blog","Professional"],
+    }
+};
+
+export default defaultWebSettings;
