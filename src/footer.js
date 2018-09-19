cc9ad2149d1b6da929ba7732a0e47d0fb27d53e2
Gaurav-Kumar-Rana <32073848+Gaurav-Kumar-Rana@users.noreply.github.com>
2018-09-19 19:21:50 +0530

    Footer Styles Added

--- src/footer.js
+++ src/footer.js
@@ -8,17 +8,35 @@ import ListItem from '@material-ui/core/ListItem';
 
 import {FooterIcon} from './utility';
 
-const style = {
+const style = theme => ({
     root:{
         flexGrow: 1,
-        padding:16
+        padding:16,
+        textAlign: "center",
+        backgroundColor: "antiquewhite",
+        marginTop: 30
+    },
+    footHeading:{
+        marginBottom:30
+    },
+    footSubHeading:{
+        textDecoration: "underline"
+    },
+    sectionUl:{
+        display: "inline-table",
+        [theme.breakpoints.down('sm')]: {
+            marginRight: 0
+        },
+        [theme.breakpoints.up('md')]: {
+            marginRight: 50
+        }
     },
     sectionList:{
         width: 64,
         display: "inline-flex",
         paddingLeft:0
     }
-};
+});
 class Footer extends React.Component {
     render(){
         const {classes,metadata,settings} = this.props;        
@@ -26,19 +44,20 @@ class Footer extends React.Component {
         let index = 0;
         return(
             <div variant="div" className={classes.root}>
-                <Typography variant="title">{footer.title}</Typography>
+                <Typography variant="title" className={classes.footHeading}>{footer.title}</Typography>
                 {
                     footer.sections.map((sec,key) => (
-                        <List key={key}>
-                            <Typography variant="body2">{sec}</Typography>
-                            {   metadata.footer[sec.toLocaleLowerCase()].length ?
+                        metadata.footer[sec.toLocaleLowerCase()].length ? 
+                        <List key={key} className={classes.sectionUl}>
+                            <Typography variant="body2" className={classes.footSubHeading}>{sec}</Typography>
+                            {
                                 metadata.footer[sec.toLocaleLowerCase()].map((sol) =>(
                                     <ListItem key={index++} className={classes.sectionList}>
                                         <FooterIcon type={sol.name.toLocaleLowerCase()} title={sol.name} url={sol.url}/>
                                     </ListItem>
-                                )) : null
+                                ))
                             }
-                        </List>
+                        </List> : null
                     ))
                 }
             </div>
